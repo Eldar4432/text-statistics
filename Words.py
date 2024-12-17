1b3075e5ff520ea4801454a288a646b7747570f5
Eldar4432 <aibekov4432@gmail.com>
2024-12-17 13:16:57 +0100

    changins comments

--- Words.py
+++ Words.py
@@ -1,19 +1,19 @@
 import re
 
-# Открываем файл с текстом
-file_path = '17.txt'  # Ваш файл называется 17.txt
+# Open a file with text
+file_path = '17.txt'
 
 with open(file_path, 'r', encoding='utf-8') as file:
     text = file.read()
 
-# Подсчёт предложений
+# Counting proposals
 sentences = re.split(r'[.!?]', text)
-num_sentences = len([s for s in sentences if s.strip()])  # Проверка на пустые предложения
+num_sentences = len([s for s in sentences if s.strip()])  # Checking for empty sentences
 
-# Подсчёт слов
-words = re.findall(r'\b\w+\b', text)  # \b - граница слова, \w+ - символы, составляющие слово
+# Word count
+words = re.findall(r'\b\w+\b', text)  # \b - main words, \w+ - symbol
 num_words = len(words)
 
-# Вывод результатов
+# Output of results
 print(f'Количество предложений: {num_sentences}')
 print(f'Количество слов: {num_words}')
